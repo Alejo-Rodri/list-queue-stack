f65e15c568211fbfaf87d534556d9a4ac88f37bd
Alejandro Rodriguez <alejandrorodriguezq0@gmail.com>
2025-08-22 07:47:34 -0500

    initial impl

--- src/main/java/com/training/data/structures/NodeImpl.java
+++ src/main/java/com/training/data/structures/NodeImpl.java
@@ -3,23 +3,31 @@ package com.training.data.structures;
 import com.training.interfaces.INode;
 
 public class NodeImpl<T> implements INode<T> {
-    @Override
-    public T getValue() {
-        return null;
+    private T value;
+    private INode<T> next;
+
+    public NodeImpl(T value) {
+        this.value = value;
+        next = null;
     }
 
     @Override
-    public void setValue(Object value) {
+    public T getValue() {
+        return value;
+    }
 
+    @Override
+    public void setValue(T value) {
+        this.value = value;
     }
 
     @Override
     public INode<T> getNext() {
-        return null;
+        return next;
     }
 
     @Override
     public void setNext(INode<T> next) {
-
+        this.next = next;
     }
 }
--- src/main/java/com/training/data/structures/SinglyLinkedListImpl.java
+++ src/main/java/com/training/data/structures/SinglyLinkedListImpl.java
@@ -1,36 +1,53 @@
 package com.training.data.structures;
 
+import com.training.interfaces.INode;
 import com.training.interfaces.ISinglyLinkedList;
 
 public class SinglyLinkedListImpl<T> implements ISinglyLinkedList<T> {
+    private INode<T> head;
+    private INode<T> tail;
+    private int size;
+
+    public SinglyLinkedListImpl(T element) {
+        head = new NodeImpl<>(element);
+        tail = head;
+        size = 1;
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size <= 0;
     }
 
     @Override
     public T first() {
-        return null;
+        return head.getValue();
     }
 
     @Override
     public T last() {
-        return null;
+        return tail.getValue();
     }
 
     @Override
     public void addFirst(T element) {
-
+        var newest = new NodeImpl<>(element);
+        newest.setNext(head);
+        head = newest;
+        size++;
     }
 
     @Override
     public void addLast(T element) {
-
+        var newest = new NodeImpl<>(element);
+        tail.setNext(newest);
+        tail = newest;
+        size++;
     }
 
     @Override
